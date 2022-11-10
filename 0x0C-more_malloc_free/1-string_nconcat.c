1cba7324b9de361325ff245899477fd534c4fd2f
asmauduhu <asmauduhu@gmail.com>
2022-11-10 13:52:40 -0800

    moremallocextra

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,44 @@
 #include "main.h"
-#include <stddef.h>
-#include <stdio.h>
-#include <stdlib.h>
+
 /**
-* string_nconcat - concatenates two strings
-* @n: length of s2
-* @s1: string one
-* @s2: string two
-* Return: return null
+*string_nconcat - Concatenates two strings using at
+*most an inputted number of bytes.
+*@s1: The first string.
+*@s2: The second string.
+*@n: The maximum number of bytes of s2 to concatenate to s1.
+*
+*Return: If the function fails - NULL.
+*Otherwise - a pointer to the concatenated space in memory.
 */
+
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *s3;
-	int count1 = 0; /*This is to count the numbber of characters in s1*/
-	int count2 = 0; /*This is to count the number of characters in s2*/
-	int index1;
-	unsigned int index2;
-	unsigned int *ptr;
+	char *concat;
+	unsigned int len = n, index;
 
 	if (s1 == NULL)
-	s1 = "";
-	else if (s2 == NULL)
-	s2 = "";
-	ptr = &n;
-	while (*(s1 + count1) != '\0')
-	{
-		count1++;
-	}
-	while (*(s2 + count2) != '\0')
-	{
-		count2++;
-	}
-	s3 = (char *)malloc(sizeof(*s3) * (count1 + count2 + 1));
-	if (s3 == NULL)
-	return (NULL);
-	else if (ptr == NULL)
-	return (s1);
-	else
-		for (index1 = 0; index1 <= count1; index1++)
-		{
-			*(s3 + index1) = *(s1 + index1);
-		}
-		for (index2 = 0; index2 <= n; index2++)
-		{
-			*((s3 + count1) + index2) = *(s2 + index2);
-		}
-	return (s3);
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (index = 0; s1[index]; index++)
+		len++;
+
+	concat = malloc(sizeof(char) * (len + 1));
+
+	if (concat == NULL)
+		return (NULL);
+
+	len = 0;
+
+	for (index = 0; s1[index]; index++)
+		concat[len++] = s1[index];
+
+	for (index = 0; s2[index] && index < n; index++)
+		concat[len++] = s2[index];
+
+	concat[len] = '\0';
+
+	return (concat);
 }
